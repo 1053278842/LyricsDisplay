22993d87cab81dde59cbc699a08f7bd168d1f062
liulong <thelegendofliulong@gmail.com>
2025-04-18 07:30:58 +0800

    留档spotify，准备兼容酷狗

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LyricsDisplay
 获取各类APP中运行的歌词，同步到树莓派中进行展示
+
+## 软连接
+1.与安卓项目集成：
+
+    ```bash
+    mklink /j "C:\Users\10532\AndroidStudioProjects\LyricHud\app\src\main\python\lyrics" "C:\mycode\vscode\lyrics" 
+    ```
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from lyrics.SpotifyLyricsListener import start
+# from lyrics.SpotifyLyricsListener import start
+from ll_kugou_lyric_api.core import KugouApi
+import base64
+import sys
+sys.dont_write_bytecode = True
 
-if __name__ == "__main__":
-   # 指定False则是正式环境
-   # start(False)
-   start()
+
+# if __name__ == "__main__":
+#    # 指定False则是正式环境
+#    # start(False)
+#    print(KugouApi("朵","赵雷").get_kugou_lrc())
+#    start()
+   
+def sb3(title,artist):
+   print(KugouApi(title,artist).get_kugou_lrc())
